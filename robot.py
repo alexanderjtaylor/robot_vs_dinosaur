d34fb478a7a83645e1cdd1517ebb09b7c72bf092
Alex <alexandertaylor315@gmail.com>
2022-09-19 23:39:29 -0400

    added a list of weapons that the deveolper can choose from to robot.py

--- robot.py
+++ robot.py
@@ -5,8 +5,8 @@ class Robot:
     def __init__(self, name):
         self.name = name
         self.health = 100
-        self.active_weapon = Weapon("Kitchen Knife", 10)
+        self.active_weapon = [Weapon("Kitchen Knife", 10), Weapon("Ray Gun", 25), Weapon("Lead Pipe", 5)]
 
     def attack(self, dinosaur):
-        dinosaur.health -= self.active_weapon.attack_power
-        print(f"{self.name} attacked {dinosaur.name} and did {self.active_weapon.attack_power} damage, leaving {dinosaur.name} with {dinosaur.health} health left!")
+        dinosaur.health -= self.active_weapon[1].attack_power
+        print(f"{self.name} attacked {dinosaur.name} with {self.active_weapon[1].name} and did {self.active_weapon[1].attack_power} damage, leaving {dinosaur.name} with {dinosaur.health} health left!")
